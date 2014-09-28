3ee62da84aa91c61164d99f87fe596d56417cc93
savvaschris <savvasmetal@gmail.com>
2014-09-28 20:55:49 +0300

    Adds navigation bar to pages, video as background in index header, page transition effects, fixes font size for mobile devices, replaces svg with png files

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-savvaschris.github.io
-=====================
+SavvasChris personal website
 
-Personal website
+A website that gives information about Savvas Christodoulou aka SavvasChris and showcases my web development/design/programming skills
+
+Structured as follows:
+- Welcome page - index.html
+- Curriculum vitae - cv.html
+- Misc info - about.html
+- Preview pages for future projects - RAD.html FIT.html CNT.html FFT.htmlects - RAD.html FIT.html CNT.html FFT.html
--- script.js
+++ script.js
@@ -17,6 +17,22 @@ $(function () {
         }
     });
 
+    //Smooths page transition
+    var speed = 'slow';
+    $('html, body').hide();
+    $(document).ready(function () {
+        $('html, body').fadeIn(speed, function () {
+            $('a[href], button[href]').click(function (event) {
+                var url = $(this).attr('href');
+                if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+                event.preventDefault();
+                $('html, body').fadeOut(speed, function () {
+                    window.location = url;
+                });
+            });
+        });
+    });
+
     /*Expand sub-sections*/
     var subSectionInfo = ["#masters", "#bachelors"];
 
@@ -161,25 +177,81 @@ $(document).ready(function () {
     $(window).scroll(function () {
 
         $("#cv section").each(function (i) {
-
             var top_of_object = $(this).position().top;
             var top_of_window = $(window).scrollTop();
-            console.log("top_of_object " + top_of_object + " top_of_window " + top_of_window);
-            if (top_of_window > top_of_object - 100) {
-                $(this).animate({ "opacity": '1' }, 1000);
+            if (top_of_window > top_of_object - screen.height / 2) {
+                $(this).animate({ "opacity": '1' }, 800);
             }
         });
 
         $("#index-page section").each(function (i) {
-
             var top_of_object = $(this).position().top;
             var top_of_window = $(window).scrollTop();
-            console.log("top_of_object " + top_of_object + " top_of_window " + top_of_window);
-            if (top_of_window > top_of_object - 100) {
+            if (top_of_window > top_of_object - screen.height / 2) {
                 $(this).animate({ "opacity": '1' }, 800);
             }
         });
+        
+        var top_of_object = $(".nav-begin").position().top;
+        var top_of_window = $(window).scrollTop();
+        if (top_of_window > top_of_object - 300) {
+            $("#navbar").animate({ "opacity": '1' }, 100);
+        }
+        else {
+            $("#navbar").animate({ "opacity": '0.1' }, 100);
+        }
+    });
+
+    $("#navbar").hover(function () {
+        var top_of_object = $(".nav-begin").position().top;
+        var top_of_window = $(window).scrollTop();
+        if (top_of_window < top_of_object - 300) {
+            console.log("hover in");
+            $("#navbar").animate({ "opacity": '1' }, 100);
+        }
+    }, function () {
+        var top_of_object = $(".nav-begin").position().top;
+        var top_of_window = $(window).scrollTop();
+        if (top_of_window < top_of_object - 300) {
+            console.log("hover out");
+            $("#navbar").animate({ "opacity": '0.1' }, 100);
+        }
     });
 
 });
 
+
+//Creates header background from youtube video
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: screen.height,
+        width: screen.width,
+        videoId: '6v2L2UGZJAM',
+        playerVars: {'autoplay': 1 ,'controls': 0 ,'enablejsapi': 1, 'iv_load_policy': 3, 'modestbranding': 1, 'showinfo':0, 'start':40, 'loop':1 , 'playlist':'6v2L2UGZJAM'},
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+function onPlayerReady(event) {
+    event.target.mute();
+    event.target.setPlaybackQuality('hd1080');
+    event.target.playVideo();
+}
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        event.target.setPlaybackQuality('hd1080');
+    }
+}
+$(window).resize(function () {
+    if (screen.height > screen.width)
+        $('#player').css({ "width": screen.height * 16 / 9, "height": screen.height });
+    if (screen.height < screen.width)
+        $('#player').css({ "width": screen.width, "height": screen.width * 9 / 16 });
+}).resize();
